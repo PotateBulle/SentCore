09ff209d6b9f00135d65cf0d78076ac2ebfc76da
PotateBulle <135218431+PotateBulle@users.noreply.github.com>
2026-09-02 17:53:40 +0200

    build: establish initial C++23 project foundation

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "SentCore 0.1.0\n";
+    std::cout << "Linux Endpoint Detection Engine\n";
+    std::cout << '\n';
+    std::cout << "Engine initialization successful.\n";
+
+    return 0;
+}
